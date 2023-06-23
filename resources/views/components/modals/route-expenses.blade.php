bda3aa804d6b8252b817ccb7b2e771cf20e294a0
Rafael Mattei <rafaelrmattei@gmail.com>
2023-06-23 12:14:00 -0300

    Bug at the size of the modal at small devices

--- resources/views/components/modals/route-expenses.blade.php
+++ resources/views/components/modals/route-expenses.blade.php
@@ -1,5 +1,5 @@
 <div id="modal-expenses" class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center">
-  <div class="relative w-auto my-6 mx-auto max-w-3xl">
+  <div class="relative w-auto my-6 mx-auto max-w-[90%] lg:max-w-4xl">
     <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
       <div class="modal-header flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
         <h3 class="modal-title text-2xl font-semibold text-blue-400">Despesas</h3><br>
